1504025082c72b86ec28f5aa821a9aeed5574e3e
vishpraveen <vishpraveen007@gmail.com>
2024-06-20 13:11:20 +0530

    getting started with ktor and coroutines

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,12 +4,15 @@ plugins {
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
+val ktor_version: String by project
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation("io.ktor:ktor-client-core:$ktor_version")
+    implementation("io.ktor:ktor-client-cio:$ktor_version")
     testImplementation(kotlin("test"))
 }
 
